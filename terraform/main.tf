f59aabda4ceace3813fb56f462d611fe4e942456
badrinadhb1012 <badrinadh1012@gmail.com>
2025-10-23 15:18:01 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,15 @@
 provider "aws" {
-region = "ap-south-1"
-
+region = "us-east-1"
 }
 
 resource "aws_instance" "key" {
-ami = "ami-012b9156f755804f5"
-instance_type = "t2.micro"
-key_name = "nwkpp"
+ami = "ami-0341d95f75f311023"
+instance_type = "t3.micro"
+key_name = "karthikmumbai"
 count = 1
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
-Name = "project-instance"
+Name = "project-instance1"
 Environment = "dev"
 }
 }
